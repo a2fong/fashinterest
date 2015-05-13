380d4c9b04bf7bee8965ba6ff87c87d3a0891561
Allison <a2fong.ucsd@gmail.com>
2015-05-13 10:09:47 -0700

    trying out flask-restless

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.restless import APIManager
 
 app = Flask(__name__)
-from app import views
+app.config.from_object('config')
+db = SQLAlchemy(app)
+manager = APIManager(flask_sqlalchemy_db=db)
+manager.init_app(app)
+
+from app import views,models
--- app/views.py
+++ app/views.py
@@ -1,6 +1,43 @@
-from app import app
+from app import app,models,db
+from app import app,db,manager,models
+import flask.ext.restless
+import json
+
+class Fashin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    blurb = db.Column(db.Text)
+    author = db.Column(db.String(120))
+    thumbnail_url = db.Column(db.String(120))
+    details_url = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<Fashin %r>' % (self.title)
+
+db.create_all()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    f = Fashin(title="title1", blurb="blurb1", author="author1", thumbnail_url="thumbnail_url1", details_url="details_url1")
+    db.session.add(f)
+    g = Fashin(title="title2", blurb="blurb2", author="author2", thumbnail_url="thumbnail_url2", details_url="details_url2")
+    db.session.add(g)
+    db.session.commit
+    fashin_blueprint = manager.create_api(Fashin, methods=['GET', 'POST', 'DELETE'])
+    print fashin_blueprint
+    return 'success'
+
+#
+# @app.route('/api/fashin/get.json', methods=['GET'])
+# def getFashins():
+#     f = models.Fashin(title="title1", blurb="blurb1", author="author1", thumbnail_url="thumbnail_url1", details_url="details_url1")
+#     db.session.add(f)
+#     g = models.Fashin(title="title2", blurb="blurb2", author="author2", thumbnail_url="thumbnail_url2", details_url="details_url2")
+#     db.session.add(g)
+#     db.session.commit
+#
+#     fs = models.Fashin.query.all()
+#
+#     print fs
+#     return fs
